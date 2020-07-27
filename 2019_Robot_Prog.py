62fa6df1adb990ff3696549079c94075b86d8e85
Yeo Jin Yan <jyyeo@caltech.edu>
2020-07-27 21:10:24 +0800

    full stars weee

--- 2019_Robot_Prog.py
+++ 2019_Robot_Prog.py
@@ -6,21 +6,23 @@ for i in range(T):
 	for j in range(A):
 		strategies.append(input())
 	output = ""
-	while (len(output) < 500):
+	while (len(output) < A):
 		index = len(output)
 		col  = [row[index%len(row)] for row in strategies]
-		
-		# all R, S, P appear in the same turn
-		if (any(ele == 'R' for ele in col) and
-			any(ele == 'S' for ele in col) and
-			any(ele == 'P' for ele in col) ):
-			output = "IMPOSSIBLE"
-			break
+
+		# # all R, S, P appear in the same turn
+		# if (any(ele == 'R' for ele in col) and
+		# 	any(ele == 'S' for ele in col) and
+		# 	any(ele == 'P' for ele in col) ):
+		# 	strategies = []
+		# 	output = "IMPOSSIBLE"
+		# 	break
 
 		if (any(ele == 'R' for ele in col)):
 			if (any(ele == 'S' for ele in col)):
 				if (any(ele == 'P' for ele in col)):
 					# R, S, P in the same turn
+					strategies = []
 					output = "IMPOSSIBLE"
 					break
 				else:
@@ -29,9 +31,16 @@ for i in range(T):
 					remove_index = [i for i in range(len(col)) if col[i] == 'S']
 					strategies = [strategies[i] for i in range(len(strategies)) if i not in remove_index]
 			else:
-				# R only
-				output += 'P'
-				break
+				if (any(ele == 'P' for ele in col)):
+					# R and P in the same turn
+					output += 'P'
+					remove_index = [i for i in range(len(col)) if col[i] == 'R']
+					strategies = [strategies[i] for i in range(len(strategies)) if i not in remove_index]
+				else:
+					# R only
+					strategies = []
+					output += 'P'
+					break
 		else:
 			if (any(ele == 'S' for ele in col)):
 				if (any(ele == 'P' for ele in col)):
@@ -41,13 +50,18 @@ for i in range(T):
 					strategies = [strategies[i] for i in range(len(strategies)) if i not in remove_index]
 				else:
 					# S only
+					strategies = []
 					output += 'R'
 					break
 			else:
 				if (any(ele == 'P' for ele in col)):
 					# P only
+					strategies = []
 					output += 'S'
 					break
+	if (strategies != []):
+		# not all opponents have been eliminated
+		output = "IMPOSSIBLE"
 
 	# print output
 	print("Case #{}: {}".format(i+1, output))
