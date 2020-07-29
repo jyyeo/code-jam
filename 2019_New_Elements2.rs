479ed7e816b6faf183bfef4864a04409088de909
Yeo Jin Yan <jyyeo@caltech.edu>
2020-07-29 19:18:54 +0800

    WA

--- 2019_New_Elements2.rs
+++ 2019_New_Elements2.rs
@@ -1,7 +1,6 @@
 #![allow(unused)]
 
 use std::io;
-// use std::cmp;
 
 fn gcd(a: i32, b: i32) -> i32 {
 	if (a == 0) {
@@ -17,8 +16,6 @@ fn simplify_frac(a: i32, b: i32) -> (i32, i32) {
 
 fn smaller_frac(temp_numer: i32, temp_denom: i32, 
 	old_numer: i32, old_denom: i32) -> (i32, i32) {
-	println!("smaller frac");
-	println!("upper_numer={} upper_denom={} temp_numer={} temp_denom={}",old_numer,old_denom,temp_numer,temp_denom);
 	if (temp_numer/temp_denom < old_numer/old_denom) {
 		return (temp_numer, temp_denom);
 	}
@@ -29,8 +26,6 @@ fn smaller_frac(temp_numer: i32, temp_denom: i32,
 
 fn larger_frac(temp_numer: i32, temp_denom: i32, 
 	old_numer: i32, old_denom: i32) -> (i32, i32) {
-	println!("larger frac");
-	println!("lower_numer={} lower_denom={} temp_numer={} temp_denom={}",old_numer,old_denom,temp_numer,temp_denom);
 	if (temp_numer/temp_denom > old_numer/old_denom) {
 		return (temp_numer, temp_denom);
 	}
@@ -39,7 +34,7 @@ fn larger_frac(temp_numer: i32, temp_denom: i32,
 	}
 }
 
-fn min_numer(lower_numer: i32, lower_denom: i32, 
+fn use_upperbound(lower_numer: i32, lower_denom: i32, 
 	upper_numer: i32, upper_denom: i32) -> (i32, i32) {
 	let upper_denom = if upper_numer == 1 {upper_denom+1} else {upper_denom};
 	let upper_numer = if upper_numer == 1 {upper_numer} else {upper_numer-1};
@@ -57,43 +52,24 @@ fn update_bounds(grid: &Vec<Vec<i32>>, n: usize) -> (i32, i32, i32, i32) {
 	let mut temp_denom: i32;
 
 	for j in 0..n-1 {
-		println!("j={}", j);
 		let a = grid[j][0];
 		let b = grid[j][1];
 		let c = grid[j+1][0];
 		let d = grid[j+1][1];
 
-		println!("{} {} {} {}", lower_numer, lower_denom, upper_numer, upper_denom);
 
 		if (a == c) { //d must be > b because strictly larger
-			println!("if a == c, a={} b={} c={} d={}",a,b,c,d);
 			continue;
 		}
 		if (b == d) { //c must be > a because strictly larger
-			println!("if b == d, a={} b={} c={} d={}",a,b,c,d);
 			continue;
 		}
 		if (c > a && d > b) {
-			println!("if c > a, d > b, a={} b={} c={} d={}", a,b,c,d);
 			continue;
 		}
 		// c != a AND d != b
-		println!("if c!=a and d!= b, a={} b={} c={} d={}",a,b,c,d);
 		let (mut temp_numer, mut temp_denom) = simplify_frac(c-a, b-d);
 
-		// let (mut temp_numer, mut temp_denom) = if b - d < 0 {
-		// 	larger_frac(temp_numer, temp_denom, lower_numer, lower_denom)
-		// } else {
-		// 	(lower_numer, lower_denom)
-		// };
-		// lower_numer = temp_numer;
-		// lower_denom = temp_denom;
-
-		// let (mut upper_numer, mut upper_denom) = if b - d >= 0 {
-		// 	smaller_frac(temp_numer,temp_denom, upper_numer, upper_denom)
-		// } else {
-		// 	(upper_numer, upper_denom)
-		// };
 		let x: (i32,i32) = if b - d < 0 {
 			larger_frac(temp_numer,temp_denom, lower_numer, lower_denom)
 		} else {
@@ -110,10 +86,7 @@ fn update_bounds(grid: &Vec<Vec<i32>>, n: usize) -> (i32, i32, i32, i32) {
 		upper_numer = if b-d > 0 {x.0} else {upper_numer};
 		upper_denom = if b-d > 0 {x.1} else {upper_denom};
 
-		println!("{} {} {} {}", lower_numer, lower_denom, upper_numer, upper_denom);
-		println!("lower limit: {} upper limit:{}", lower_numer as f32/lower_denom as f32, upper_numer as f32/upper_denom as f32);
 		if j == n-2 {
-			println!("{} {} {} {}", lower_numer, lower_denom, upper_numer, upper_denom);
 			return (lower_numer, lower_denom, upper_numer, upper_denom);
 		}
 	}	
@@ -153,28 +126,27 @@ fn main() {
 		let temp_denom: i32;
 
 		let (lower_numer, lower_denom, upper_numer, upper_denom) = update_bounds(&grid, n);
-		println!("{} {} {} {}", lower_numer, lower_denom, upper_numer, upper_denom);
 
-		let lowerbound = lower_numer/lower_denom;
-		let upperbound = upper_numer/upper_denom;
+		let lowerbound = lower_numer as f32/lower_denom as f32;
+		let upperbound = upper_numer as f32/upper_denom as f32;
 		let wc = 0i32;
 		let wj = 0i32;
 		if (lowerbound >= upperbound) {
 			println!("Case #{}: IMPOSSIBLE", i);
 		}
 		else {
-			if (upperbound > 1 && lowerbound < 1) {
+			if (lowerbound < 1.0 && upperbound > 1.0) {
 				let wc = 1i32;
 				let wj = 1i32;
 				println!("Case #{}: {} {}", i, wc, wj);
 			}
-			else if (lowerbound > 1) {
+			else if (lowerbound >= 1.0) {
 				let wc = lower_denom;
-				let wj = lower_numer;
+				let wj = lower_numer + 1;
 				println!("Case #{}: {} {}", i, wc, wj);
 			}
-			else {
-				let (wj, wc) = min_numer(lower_numer, lower_denom, upper_numer, upper_denom);
+			else { // LB and UB < 1.0
+				let (wj, wc) = use_upperbound(lower_numer, lower_denom, upper_numer, upper_denom);
 				if (wj, wc) == (0,0) {
 					println!("Case #{}: IMPOSSIBLE", i);
 				}
