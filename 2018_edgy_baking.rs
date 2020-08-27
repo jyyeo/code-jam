0a34d01c77482cfda60d1c39e98ba9e7b1a23786
Yeo Jin Yan <jinyan@Yeos-MacBook-Pro.local>
2020-08-27 22:11:58 +0800

    ??

---
+++ 2018_edgy_baking.rs
@@ -0,0 +1,40 @@
+use std::io;
+
+fn main() {
+	print!("testing\n");
+	// let cin = io::stdin();
+	// let mut input = String::new();
+	// cin.read_line(&mut input).unwrap();
+	// let t: i32 = input.trim().parse().unwrap();
+	// input.clear();
+
+	// for i in 1..t+1 {
+	// 	println!("i:{}", i);
+	// 	cin.read_line(&mut input).unwrap();
+	// 	let values = input
+	// 		.split_whitespace()
+	// 		.map(|x| x.parse::<i32>())
+	// 		.collect::<Result<Vec<i32>, _>>()
+	// 		.unwrap() ;
+	// 	let n: i32 = values[0];
+	// 	let p: i32 = values[1];
+	// 	input.clear();
+	// 	values.clear();
+
+	// 	let mut curr_p: i32 = 0;
+	// 	for j in 0..n {
+	// 		cin.read_line(&mut input).unwrap();
+	// 		let values = input
+	// 			.split_whitespace()
+	// 			.map(|x| x.parse::<i32>())
+	// 			.collect::<Result<Vec<i32>, _>>()
+	// 			.unwrap() ;
+	// 		let w: i32 = values[0];
+	// 		let h: i32 = values[1];
+	// 		input.clear();
+	// 		values.clear();
+	// 		curr_p += w;
+	// 		curr_p += h;
+	// 	}
+	// }
+}
