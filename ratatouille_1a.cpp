7c1d796e858be555931110e7453bbcd47839ca57
Yeo Jin Yan <jinyan@Yeos-MacBook-Pro.local>
2020-08-30 15:11:44 +0800

    taking a break from this :(

---
+++ ratatouille_1a.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+	int T;
+	scanf("%d", &T);
+
+	for (int i = 0; i < T; i++) {
+		int N, P;
+		scanf("%d %d", &N, &P);
+
+		int weights[50];
+		for (int j = 0; j < N; j++) {
+			scanf("%lf", &weights[j]);
+		}
+
+		for (int j = 0; j < N; j++) {
+			int packages[50];
+			for (int k = 0; k < P; k++) {
+				int package;
+				scanf("%d", &package);
+				
+			}
+			
+		}
+
+	}
+}
